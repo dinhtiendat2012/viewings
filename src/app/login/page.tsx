b366c261d64e4a27a9f122dc422d456af3a470cc
Dat Dinh Tien <datdthe163003@fpt.edu.vn>
2024-09-19 17:20:03 +0700

    update

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/Header";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Header />
+      <div className="loginBg">
+        <div className="loginForm">1</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ProfilePage() {
+  return (
+    <div className="profileBg">
+      <div className="online"></div>
+      <div className="info"></div>
+      <div className="notif">Notif</div>
+    </div>
+  );
+}
